7dd3112cd87a16e57db03e4669f2357ce12f8683
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-02-28 18:35:44 -0500

    functions that can take a location and return the weather data for that location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+
+const API_KLJUC = "68Z86M7ZXLK98UL8VC3ZEWNRJ";
+
+let trenutniPodaci = null;
+
+async function dohvatiVreme() {
+  try {
+ 
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=${API_KLJUC}`;
+
+    const odgovor = await fetch(url);
+
+    if (!odgovor.ok) {
+      throw new Error("Grad nije pronađen");
+    }
+    const podaci = await odgovor.json();
+    console.log(podaci)
+
+  } catch (greska) {
+    console.log(greska.message) 
+     
+  } 
+}
+dohvatiVreme();
